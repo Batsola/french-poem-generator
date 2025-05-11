6ff0c68e3306ab82a23f966b8fcc6a29ba94d65c
Batsola <drawstsu@gmail.com>
2025-05-11 10:40:55 +0300

    fixed blinking effect

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function generatePoem(event) {
   let apiKey = "0b94ffa0fbab4a7377346039a787t35o";
   let prompt = `Write a French poem about ${instructionsInput.value}, in 3 stanzas of 4 lines each in basic HTML. Use poetic vocabulary, and a  rhymed structure.`;
   let context =
-    "You are a romantic French poet, a master of poetic expression. You write elegant and emotionally resonant poems in French, strictly following the user's instructions. Your response must be formatted in clean, basic HTML using <p> for stanzas and <br> for line breaks. Only return the poem in HTML. At the end of the poem, add a final line centered and italicized that reads: <p style='text-align: center; font-style: italic;'><strong>SheCodes AI</strong></p>";
+    "You are a romantic French poet, a master of poetic expression. You write elegant and emotionally resonant poems in French, strictly following the user's instructions. Your response must be formatted in clean, basic HTML using <p> for stanzas and <br> for line breaks. At the end of the poem, add a final line centered and italicized that reads: <p style='text-align: center; font-style: italic;'><strong>SheCodes AI</strong></p>";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
